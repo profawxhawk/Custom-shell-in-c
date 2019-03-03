9fb17e15288e12be4a9d3cabbf8677990352b730
profawxhawk <35334693+profawxhawk@users.noreply.github.com>
2019-03-03 23:41:20 +0530

    have to implement run_single_command

--- shell.c
+++ shell.c
@@ -1,60 +1,65 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
- #include <sys/wait.h>
+#include <sys/wait.h>
 #define GREEN "\x1b[32m"
 #define BLUE "\x1b[34m"
 #define RESET "\x1b[0m"
 char hostname[1024];
 char *user;
 char cwd_path[1024];
-char *args[10000];
-char *take_input(){
-    char *in_string=malloc(255);
-    if(in_string==NULL){
+char *take_input()
+{
+    char *in_string = malloc(255);
+    if (in_string == NULL)
+    {
         printf("Unable to read command as no space available in heap");
         exit(0);
     }
-    else{
-        int i=0;
+    else
+    {
+        int i = 0;
         char chary;
-        int count=1;
-        long size=255;
-        chary=getchar();
-        while(chary!='\n'){          // replace with actual enter key detection
-            if((int)chary==-1){
+        int count = 1;
+        long size = 255;
+        chary = getchar();
+        while (chary != '\n')
+        { // replace with actual enter key detection
+            if ((int)chary == -1)
+            {
                 break;
             }
-            in_string[i]=chary;
+            in_string[i] = chary;
             i++;
-            if(i==255){
+            if (i == 255)
+            {
                 count++;
-                size=count*size;
-                in_string=realloc(in_string,size);
+                size = count * size;
+                in_string = realloc(in_string, size);
             }
-            chary=getchar();
+            chary = getchar();
         }
-        in_string[i]='\0';
+        in_string[i] = '\0';
     }
     return in_string;
 }
 void display_path()
 {
-    printf(GREEN "%s@%s" RESET ":" BLUE "%s" RESET "$",user,hostname,cwd_path);
+    printf(GREEN "%s@%s" RESET ":" BLUE "%s" RESET "$", user, hostname, cwd_path);
 }
-int Parse_input(char *input,char * delimiters,char **output){
-    
-    output[0]=strtok(input,delimiters);
-    
-    int i=0;
+int Parse_input(char *input, char *delimiters, char **output)
+{
+
+    output[0] = strtok(input, delimiters);
+
+    int i = 0;
 
     while ((output[i]) != NULL)
     {
         i++;
         (output[i]) = strtok(NULL, delimiters);
-        
     }
     char *ret_1 = getcwd(cwd_path, 1024);
     if (ret_1 == NULL)
@@ -63,16 +68,20 @@ int Parse_input(char *input,char * delimiters,char **output){
         exit(1);
     }
     return i;
-  
 }
-void run_single_command(char *input){
-    printf("%s\n",input);
+void run_single_command(char *input)
+{
+    char *args[10000];
+    int args_size = Parse_input(input, " ", args);
+   
 }
-void pipe_input(char **input,int size){
+void pipe_input(char **input, int size)
+{
     int pid;
     int fd[2];
     int ret;
-    for(int i=0;i<size;i++){
+    for (int i = 0; i < size; i++)
+    {
         ret = pipe(fd);
         if (ret == -1)
         {
@@ -82,12 +91,13 @@ void pipe_input(char **input,int size){
         pid = fork();
         if (pid == 0)
         {
-            dup2(fd[0],0);
-            if(i!=(size-1)){
-                dup2(fd[1],1);    // close and dup gives race conditions so use dup2
+            dup2(fd[0], 0);
+            if (i != (size - 1))
+            {
+                dup2(fd[1], 1); // close and dup gives race conditions so use dup2
             }
             close(fd[0]);
-            run_single_command(*(input+i));
+            run_single_command(*(input + i));
             exit(1);
         }
         else if (pid > 0)
@@ -103,29 +113,32 @@ void pipe_input(char **input,int size){
     close(fd[0]);
     close(fd[1]);
 }
-void start_shell(){
-    int status=1;
-   
-    do{
-            
-        display_path();
-        int pipe_req=0;
-        char *input_comand=take_input();
-        int args_size=Parse_input(input_comand," | ",args);
-        if(args_size>1){
-            pipe_req=1;
-            pipe_input(args,args_size);
-        }
-        else{
+void start_shell()
+{
+    int status = 1;
 
+    do
+    {
+
+        display_path();
+        int pipe_req = 0;
+        char *input_comand = take_input();
+        char *args[10000];
+        int args_size = Parse_input(input_comand, "|", args);
+        if (args_size > 1)
+        {
+            pipe_req = 1;
+            pipe_input(args, args_size);
         }
-        
-    }while(status);
-    
-    
-   
+        else
+        {
+            run_single_command(*args);
+        }
+
+    } while (status);
 }
-int initialise_var(){
+int initialise_var()
+{
     user = getenv("USER");
     if (user == NULL)
     {
@@ -145,7 +158,8 @@ int initialise_var(){
         exit(1);
     }
 }
-int main(){
+int main()
+{
     initialise_var();
     start_shell();
 }
